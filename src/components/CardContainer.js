53ecd0af951a89839dec3f177cb522ed8af1c353
andy le <anhlt1983@gmail.com>
2018-07-11 00:17:38 +1000

    mockup UI item

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import CardItem from './CardItem';
+import { CardColumns } from 'reactstrap';
 
 export default class CardContainer extends Component {
    render() {
       return (
          <Container>
             <Row>
-               <Col>{this.props.children}</Col>
-            </Row> 
+               <Col>
+                  <Container>
+                     <Row style={{justifyContent: "center"}}>
+                        Results
+                     </Row>
+                     <Row>
+                        <Col>{this.props.children}</Col>
+                     </Row>
+                     <Row mb="2">
+                        <Col>{this.props.children}</Col>
+                     </Row>
+                     <Row>
+                        <Col>{this.props.children}</Col>
+                     </Row>                         
+                  </Container>
+               </Col>
+               <Col>
+                  <Container>
+                     <Row style={{justifyContent: "center"}}>
+                        Saved Properties
+                     </Row>
+                     <Row>
+                        <Col>{this.props.children}</Col>
+                     </Row>                         
+                  </Container>
+               </Col>
+            </Row>
          </Container>
       );
    }
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,25 +5,40 @@ import {
    CardText,
    CardBody,
    CardTitle,
-   CardSubtitle
+   CardSubtitle,
+   CardHeader,
+   CardFooter
 } from 'reactstrap';
 import React from "react";
 import ReactDOM from "react-dom";
 
 const CardIem = () => {
    return (
-      <div>
+      <div  style={styles.cardStyle}>
          <Card>
-            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+            <CardHeader style={styles.cardHeaderStyle}>
+               <CardImg style={styles.cardLogoStyle} src="http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif&w=2&h=2" /> Properties
+            </CardHeader>
             <CardBody>
-               <CardTitle>Card title</CardTitle>
-               <CardSubtitle>Card subtitle</CardSubtitle>
-               <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-               <Button>Button</Button>
+               <CardImg top width="100%" src="http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg" alt="Card image cap" />          
             </CardBody>
+            <CardFooter styele={styles.cardFooterStyle}>$100,000</CardFooter>
          </Card>
       </div>
    );
+};
+
+const styles = {
+   cardStyle: {
+      margin: "10px"
+   },
+   cardHeaderStyle: {
+      background: "#cdc", // TODO: need change color
+      display: "flex d-flex"
+   },
+   cardLogoStyle: {
+      width: "100px",
+   }
 };
 
 export default CardIem;
