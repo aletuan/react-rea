82582de54910a1cf6df4ef5bd21d604b0045fbe6
andy le <anhlt1983@gmail.com>
2018-07-11 13:28:15 +1000

    adding css file for card container

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,22 +1,23 @@
 import React, {Component} from 'react';
 import { Container, Row, Col, CardColumns, Card, CardHeader, CardBody} from 'reactstrap';
 import CardItem from './CardItem';
+import './CardContainer.css';
 
 const CardContainer = ({title, cardItems}) => {
    return (
       <div>
-      <div style={{display: "flex", justifyContent: "center"}}>{title}</div>
-      <Container style={{borderStyle: "dotted", borderWidth: "1px"}}>
-      {                        
-         cardItems.map(item => {
-            // destructruct nested object
-            const {id, price, mainImage, agency} = item;
-            const {logo, brandingColors: {primary}} = agency;
+         <div className="cardContainerHeaderStyle">{title}</div>
+         <Container className="cardContainerBodyStyle">
+         {                        
+            cardItems.map(item => {
+               // destructruct nested object
+               const {id, price, mainImage, agency} = item;
+               const {logo, brandingColors: {primary}} = agency;
 
-            return <CardItem key={id} id={id} price={price} mainImage={mainImage} logo={logo} primary={primary} />;
-         })
-      }                  
-      </Container>
+               return <CardItem key={id} id={id} price={price} mainImage={mainImage} logo={logo} primary={primary} />;
+            })
+         }                  
+         </Container>
       </div>
    );
 };
