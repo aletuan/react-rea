4cfc5cdd7059c4fd94d6f3086beb358561d8f7fb
andy le <anhlt1983@gmail.com>
2018-07-12 02:01:53 +1000

    testing page container with mock redux

---
+++ src/__tests__/PageContainer.test.js
@@ -0,0 +1,47 @@
+import configureStore from 'redux-mock-store';
+import React from 'react';
+import { shallow } from 'enzyme';
+import PageContainer from '../components/PageContainer';
+import { CardContainer } from '../components/CardContainer';
+import { Container, Row, Col} from 'reactstrap';
+
+describe('PageContainer layout test', () => {
+
+   const initialState = {
+      results: [{
+         "price": "$726,500",
+         "agency": {
+             "brandingColors": {
+                 "primary": "#ffe512"
+             },
+             "logo": "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
+         },
+         "id": "1",
+         "mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
+      }],
+      saved: [{
+         "price": "$726,500",
+         "agency": {
+             "brandingColors": {
+                 "primary": "#ffe512"
+             },
+             "logo": "http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif"
+         },
+         "id": "1",
+         "mainImage": "http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg"
+      }],
+   };   
+   const mockStore = configureStore();
+   let store;
+
+   beforeEach(() => {
+      store = mockStore(initialState);
+   });
+
+   it('should have two columns of properties', ()=> {
+      const wrapper = shallow(<PageContainer store={store} />);
+      const answer = wrapper.html().match(/cardContainerHeaderStyle/g).length;
+      expect(answer).toBe(2);
+   });
+
+});
