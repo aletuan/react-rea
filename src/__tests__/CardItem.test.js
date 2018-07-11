1826fac3341169cd312681605addc5d2cd8c739d
andy le <anhlt1983@gmail.com>
2018-07-11 22:50:21 +1000

    adding test case of CardItem

--- src/__tests__/CardItem.test.js
+++ src/__tests__/CardItem.test.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CardIem from '../components/CardItem';
+import { CardBody, CardImg } from 'reactstrap';
 
 describe('CardItem layout test', () => {
 
+   let wrapper;
+   let item = {
+      logo: "abc",
+      mainImage: "abc",
+      price: "$123,789"
+   };
+
    beforeEach(() => {
-      console.log('running before each testing layout');
+      wrapper = shallow(<CardIem logo={item.logo} mainImage={item.mainImage} price={item.price} />);
    });
 
    it('should display logo', () => {
-      const wrapper = shallow(<CardIem logo="abc" />);
       const answer = wrapper.find('.cardHeaderStyle');
-      expect(answer.find('.cardLogoStyle').prop('src')).toBe("abc");
+      expect(answer.find('.cardLogoStyle').prop('src')).toBe(item.logo);
    });
 
-   it('should display main image');
-   it('should display pricing');
+   it('should display main image', () => {
+      const answer = wrapper.find('CardBody');
+      expect(answer.find(CardImg).prop('src')).toBe(item.mainImage);
+   });
+
+   it('should display pricing', () => {
+      const answer = wrapper.find('.cardFooterStyle');
+      expect(answer.children().text()).toBe(item.price);
+   });
 });
